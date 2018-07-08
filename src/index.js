c1cb73984f4db669e0f698257aae57b74e9e687c
Danielle H <daheber@gmail.com>
2018-07-08 10:49:58 -0700

    basic styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Game from './components/Game';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
